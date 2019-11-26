358b22ffb82cd8680a8d0365ca78c5c34d582bf3
Nick Thompson <nick.thompson@booking.com>
2019-11-26 14:35:08 +0100

    updating

--- snippets/Pandas.py
+++ snippets/Pandas.py
@@ -1,14 +1,6 @@
-# clean headers of pandas dataframes
 def clean_pandas(df):
-    df.columns = df.columns\
-		# remove leading and trailing space
-		.str.strip()\
-                #make lower case
-                .str.lower()\
-                # replace spaces with '_'
-                .str.replace(' ', '_')\
-                # remove open parenthesis
-                .str.replace('(', '')\
-                # remove close parenthesis
-                .str.replace(')', '')
-
+    df.columns = df.columns.str.strip()\
+    .str.lower()\
+    .str.replace(' ', '_')\
+    .str.replace('(', '')\
+    .str.replace(')', '')
